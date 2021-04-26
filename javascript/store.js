2a9a53590fc72293261a393b861ec40602892b07
vyshali1410 <mathukumallivyshali@gmail.com>
2021-04-26 18:00:38 -0400

    updated

--- javascript/store.js
+++ javascript/store.js
@@ -116,7 +116,7 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    alert(imageSrc)
+   //alert(imageSrc)
     addItemToCart(title, price, imageSrc)
     updateCartTotal()
 }
